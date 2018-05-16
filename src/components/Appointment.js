2658a013ac42ba51025cb7402aa52c9c693fb3f3
Austin Randall <randall@Randall.local>
2018-05-16 13:41:57 -0700

    fixed sent picts

--- src/components/Appointment.js
+++ src/components/Appointment.js
@@ -3,6 +3,9 @@ import Header from './Header'
 import Footer from './Footer'
 import axios from 'axios'
 import {NavLink} from 'react-router-dom'
+import Images from './Images'
+// import image from '../assets/Classy-K&B-image-placeholder.jpg'
+import image from '../assets/Spinner-1.1s-139px.gif'
 var Recaptcha = require('react-recaptcha');
 
 class Appointment extends Component {
@@ -27,7 +30,7 @@ class Appointment extends Component {
       interestedIn: [],
       comments: '',
       verified: false,
-      sent: false,
+      sent: true,
     }
     this.input=this.input.bind(this)
     this.reset=this.reset.bind(this)
@@ -254,40 +257,52 @@ class Appointment extends Component {
           <div className='content' id='content-sent'>
             <div className='header'>
               <h3>Success!!!</h3>
-              <p>THank you for contacting us!</p>
+              <p>Thank you for contacting us!</p>
               <p>You should hear back from our representative shortly. In the meantime, browse our products and see why our customers speak so highly of our work!</p>
             </div>
             <div className='pict-links'>
-              <div className='link-details'>
-              <NavLink to='/kitchens'><div><img alt='Kitchens' src='https://www.dropbox.com/s/yn8opdrg9hhhg06/Landing-kitchen-%281%29.jpg?raw=1' /></div></NavLink>
-                Kitchens
-                <p>At Classy Kitchen and Bath our professional designers create custom kitchens designed just for you. Create a custom kitchen that fits your style, budget and space by scheduling your free in-home consultation today!</p>
-              </div>
-              <div className='link-details'>
-                <NavLink to='/countertops'><div><img alt='Countertops' src='https://www.dropbox.com/s/fwqcxsaa36rq3m3/Cambria-Brittanica-%281%29.jpg?raw=1' /></div></NavLink>
-                Countertops
-                <p>Choose from a selection of granite, marble or quartz to find the perfect countertop material for your new project. At Classy Kitchen and Bath, we can help you find the perfect countertop material for your kitchen and bathroom. Shop now!</p>
-              </div>
-              <div className='link-details'>
-                <NavLink to='/baths'><div><img alt='Baths' src='https://www.dropbox.com/s/km1mv00swlpa2im/1-Classic-Tile-and-Glass-Bathroom-resized.jpg?raw=1' /></div></NavLink>
-                Baths
-                <p>At Classy Kitchen and Bath, we create custom bathrooms designed especially for your home, style and budget. Choose from a large selection of styles, materials and hardware to create your dream bathroom! </p>
-              </div>
-              <div className='link-details'>
-                <NavLink to='/doors'><div><img alt='Doors' src='https://www.dropbox.com/s/9gy2vt3jrd0f7h0/26-The-Highland-resized.jpg?raw=1' /></div></NavLink>
-                Cabinet Doors
-                <p>At Classy Kitchen and Bath you can choose from a selection of different cabinet door styles for your custom kitchen or bath project. Whether you’re searching for modern cabinets or traditional cabinets, Classy Kitchen and Bath has it all. </p>
-              </div>
-              <div className='link-details'>
-                <NavLink to='/our-process'><div><img alt='Our Process' src='https://www.dropbox.com/s/j6z2zm5vwynxjha/Landing-Process-%281%29.jpg?raw=1' /></div></NavLink>
-                Our Process
-                <p>At Classy Kitchen and Bath, the process of designing and building your new kitchen or bathroom is all about you. Our professional designers are passionate about creating a custom kitchen and/or bathroom that reflects your home, style and budget. </p>
-              </div>
-              <div className='link-details'>
-                <NavLink to='/showroom'><div><img alt='Our Process' src='https://www.dropbox.com/s/anscz4jg8sis97i/chandler-showroom.jpg?raw=1' /></div></NavLink>
-                Visit a Showroom
-                <p>Come to a local showroom to meet with an expert designer, see our custom build displays, and see how we can create the custom kitchen or bath that fits your style perfectly.</p>
-              </div>
+            <div className='link-details'>
+            <NavLink to='/kitchens'><div>
+                <Images srcPreload={image} srcLoaded='https://www.dropbox.com/s/6z7d22bjm21i2dk/5-French-Grey-Textured-Kitchen.jpg?raw=1' name='Kitchens' />
+              </div></NavLink>
+              Kitchens
+              <p>At Classy Kitchen and Bath our professional designers create custom kitchens designed just for you. Create a custom kitchen that fits your style, budget and space by scheduling your free in-home consultation today!</p>
+            </div>
+            <div className='link-details'>
+              <NavLink to='/countertops'><div>
+                <Images srcPreload={image} srcLoaded='https://www.dropbox.com/s/fwqcxsaa36rq3m3/Cambria-Brittanica-%281%29.jpg?raw=1' name='Countertops' />
+              </div></NavLink>
+              Countertops
+              <p>Choose from a selection of granite, marble or quartz to find the perfect countertop material for your new project. At Classy Kitchen and Bath, we can help you find the perfect countertop material for your kitchen and bathroom. Shop now!</p>
+            </div>
+            <div className='link-details'>
+              <NavLink to='/baths'><div>
+                <Images srcPreload={image} srcLoaded='https://www.dropbox.com/s/km1mv00swlpa2im/1-Classic-Tile-and-Glass-Bathroom-resized.jpg?raw=1' name='Bathrooms' />
+              </div></NavLink>
+              Baths
+              <p>At Classy Kitchen and Bath, we create custom bathrooms designed especially for your home, style and budget. Choose from a large selection of styles, materials and hardware to create your dream bathroom! </p>
+            </div>
+            <div className='link-details'>
+              <NavLink to='/doors'><div>
+                <Images srcPreload={image} srcLoaded='https://www.dropbox.com/s/nl2e7kmth88ab60/21-Milano-Vanity-in-Gunstock-Walnut.jpg?raw=1' name='Cabinet Doors' />
+              </div></NavLink>
+              Cabinet Doors
+              <p>At Classy Kitchen and Bath you can choose from a selection of different cabinet door styles for your custom kitchen or bath project. Whether you’re searching for modern cabinets or traditional cabinets, Classy Kitchen and Bath has it all. </p>
+            </div>
+            <div className='link-details'>
+              <NavLink to='/our-process'><div>
+                <Images srcPreload={image} srcLoaded='https://www.dropbox.com/s/j6z2zm5vwynxjha/Landing-Process-%281%29.jpg?raw=1' name='Our Process' />
+              </div></NavLink>
+              Our Process
+              <p>At Classy Kitchen and Bath, the process of designing and building your new kitchen or bathroom is all about you. Our professional designers are passionate about creating a custom kitchen and/or bathroom that reflects your home, style and budget. </p>
+            </div>
+            <div className='link-details'>
+              <NavLink to='/showroom'><div>
+                <Images srcPreload={image} srcLoaded='https://www.dropbox.com/s/anscz4jg8sis97i/chandler-showroom.jpg?raw=1' name='Showroom' />
+              </div></NavLink>
+              Visit a Showroom
+              <p>Come to a local showroom to meet with an expert designer, see our custom build displays, and see how we can create the custom kitchen or bath that fits your style perfectly.</p>
+            </div>
             </div>
           </div>
         }
